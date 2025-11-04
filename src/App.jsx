42f9e61a6cb8117becfd2eb17793938de8072c03
MuhammadNaeem001 <nk510448@gmail.com>
2025-11-04 11:27:31 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl font-bold text-blue-600">
+      Hello Naeem! 👋 Tailwind + Router + Toastify working!
+    </h1>
+  );
+}
+
+export default App;
