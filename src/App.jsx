ae821fe70401e2b9f42733c7e9afb5b0556be55a
MuhammadNaeem001 <nk510448@gmail.com>
2025-11-05 22:54:25 -0800

    navvar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-function App() {
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Appointment from './pages/Appointment.jsx';
+import Doctors from './pages/Doctors.jsx'; // ✅ check file name carefully
+import MyAppointments from './pages/MyAppointments.jsx';
+import MyProfile from './pages/MyProfile.jsx';
+import Login from './pages/Login.jsx';
+import About from './pages/About.jsx';
+import Contact from './pages/Contact.jsx';  
+import Navbar from './components/Navbar.jsx';
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold text-blue-600">
-      Hello Naeem! 👋 Tailwind + Router + Toastify working!
-    </h1>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
+    </div>
   );
-}
+};
 
 export default App;
